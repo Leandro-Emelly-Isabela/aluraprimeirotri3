983ad5849cfcb2c50646c4689e9e1a523999c61d
Leandro Castanha <116896587+Leandro-Emelly-Isabela@users.noreply.github.com>
2024-04-23 23:15:49 -0300

    Update main.js

--- js-projeto-aula-10/main.js
+++ js-projeto-aula-10/main.js
@@ -30,6 +30,7 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+    let anos = Math.floor(dias / 365);
 
     segundos %= 60;
     minutos %= 60;
